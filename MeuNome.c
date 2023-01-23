0d54874dfe2136e9cf865771ea336a55335b3352
HenriqueForte <a32771@tomazpelayo.com>
2023-01-23 14:26:54 +0000

    Update MeuNome.c

--- MeuNome.c
+++ MeuNome.c
@@ -1,3 +1,10 @@
 // Resolva neste ficheiro o seu exercício
 
+#include<stdio.h>
 
+    int main() {
+    printf("Nome: Henrique Forte\n");
+    printf("Data: 09 de agosto de 2007\n");
+    printf("Morada: Vila das Aves\n");
+    
+    }
